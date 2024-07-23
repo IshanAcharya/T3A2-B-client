841781e7fbef95565167519f06296a9181a173a7
IshanAcharya <ishanacharya@live.com>
2024-07-23 18:40:05 +1000

    Initial setup: Added basic structure to home page, app routing and set up index.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import LoginRegister from './components/LoginRegister';
-import Dashboard from './components/Dashboard';
+import Home from './pages/Home';
+import LoginRegister from './pages/LoginRegister';
 import Profile from './components/Profile';
-import TypingPractice from './components/TypingPractice';
-import FreeTyping from './components/FreeTyping';
-import ViewSessions from './components/ViewSessions';
+import TypeTutor from './pages/TypeTutor';
+import ViewSessions from './pages/ViewSessions';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/login-register" component={LoginRegister}/>
-        <Route path="/dashboard" component={Dashboard}/>
-        <Route path="/profile" component={Profile}/>
-        <Route path="/typing-practice" component={TypingPractice}/>
-        <Route path="/free-typing" component={FreeTyping}/>
-        <Route path="/view-sessions" component={ViewSessions}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/login-register" component={LoginRegister} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/view-sessions" component={ViewSessions} />
+        <Route path="/type-tutor" component={TypeTutor} />
       </Switch>
     </Router>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+
+
+function Home() {
+    return (
+        <div className="home-container">
+            <div className="logo">
+                <img src="reminder:enter logo path" alt="Type Tutor Logo" />
+            </div>
+            <p className="home-Text">
+                In today's digital age, typing speed and accuracy are crucial life skills. Don't get left behind, improve your skills with the Typee Tutor App. 
+            </p>
+            <div className="buttons">
+                <link to="/login-register"><button className="login-button">Login</button></link>
+                <link to="/login-register"><button className="register-button">Register</button></link>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
