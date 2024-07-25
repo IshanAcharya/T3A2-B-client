e265a2c22799a460542627a225e1155316cb2775
IshanAcharya <ishanacharya@live.com>
2024-07-25 20:00:31 +1000

    Added PrivateRoute component for route protection, updated home.js and app.js for routing setup, and styled home page with home.css.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from '.components/Navbar';
 import Home from './pages/Home';
 import LoginRegister from './pages/LoginRegister';
-import Profile from './components/Profile';
+import Profile from './pages/Profile';
 import TypeTutor from './pages/TypeTutor';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/login-register" component={LoginRegister} />
-          <Route path="/profile" component={Profile} />
-          <Route path="type-tutor" component={TypeTutor} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="type-tutor" component={TypeTutor} />
         </Switch>
       </div>
     </Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 import '../styles/Home.css';
 
-
-function Home() {
+const Home = () => {
     return (
-        <div className="home-container">
-            <div className="logo">
-                <img src="reminder:enter logo path" alt="Type Tutor Logo" />
-            </div>
-            <p className="home-Text">
-                In today's digital age, typing speed and accuracy are crucial life skills. Don't get left behind, improve your skills with the Typee Tutor App. 
-            </p>
-            <div className="buttons">
-                <link to="/login-register"><button className="login-button">Login</button></link>
-                <link to="/login-register"><button className="register-button">Register</button></link>
+        <div className="home">
+            <Navbar />
+            <Header />
+            <div className="home-content">
+                <p>In today's digital age, typing speed and accuracy are crucial life skills. Don't get left behind, improve your skills with the Type Tutor App.</p>
+                <div className="home-buttons">
+                    <Link to="/login-register" className="button">
+                        Login/Register
+                    </Link>
+                </div>
             </div>
         </div>
-    );
-}
+    );        
+};
+
 
 export default Home;
