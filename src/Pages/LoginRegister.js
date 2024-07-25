de2a203e164cf760e4eec68b5c816977e9589a89
IshanAcharya <ishanacharya@live.com>
2024-07-25 21:39:20 +1000

    Defined LoginRegister component and set up state variables in LoginRegister component, added initial styles for LoginRegister page background in CSS styling.

--- src/Pages/LoginRegister.js
+++ src/Pages/LoginRegister.js
@@ -1,1 +1,18 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Header from '../components/Header';
+import '../styles/LoginRegister.css';
+
+// Define LoginRegister component
+const LoginRegister = () => {
+    // State variables for login/register form
+    const [isLogin, setIsLogin] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState('');
+    const history = useHistory();
+
+
+
+    
+}
