6f6f1daee7a7efe10c0e5c9ed32f6e123893a550
IshanAcharya <ishanacharya@live.com>
2024-07-26 20:47:06 +1000

    Added profile.js for page components and profile.css for styling, added ProfileForm.js for form on page to update user email and password and ProfileForm.css for styling, and added PastSessionsTable.js to display user's previous typing sessions data on profile page in table and PastSessionsTable.css for styling.

---
+++ src/Pages/Profile.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import ProfileForm from '../components/ProfileForm';
+import PastSessionsTable from '../components/PastSessionsTable';
+import '../styles/Profile.css';
+
+
