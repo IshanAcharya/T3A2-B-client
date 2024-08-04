77ba9a7fb1dc58566cc797b41083790e575e7683
IshanAcharya <ishanacharya@live.com>
2024-08-04 12:35:32 +1000

    Updated connection to backend to render url.

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5600/api',
+    baseURL: 'https://t3a2-b-server-mi40.onrender.com',
 });
 
 export default axiosInstance;
