30fb9ecdb15c745358faf73b21c3446f840d075a
IshanAcharya <ishanacharya@live.com>
2024-08-06 01:20:12 +1000

    Updated baseURL in axiosConfig.js to new server URL.

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://t3a2-b-server-mi40.onrender.com/api',
+    baseURL: 'https://typetutor.onrender.com/api',
 });
 
 export default axiosInstance;
